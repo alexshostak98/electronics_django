95661825fac2f590ea86406a383ccda3c2723c3e
alexshostak98 <alexandr_shostak@mail.ru>
2023-07-01 13:30:40 +0300

    startapp company, create Company model

---
+++ company/error_messages.py
@@ -0,0 +1,4 @@
+ERROR_MESSAGES = {
+    "wrong_factory_debt": "Factory can't have a debt, even 0. Leave this field blank or choose another company type",
+    "should_has_supplier": "This company type should has a supplier. Please choose the supplier",
+}
---
+++ company/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+from company.error_messages import ERROR_MESSAGES
+
+
+class Company(models.Model):
+    FACTORY = 'FA'
+    DISTRIBUTOR = "DB"
+    DEALERSHIP = "DS"
+    RETAIL = "RT"
+    INDIVIDUAL = "IE"
+    ELEMENT_TYPES = [
+        (FACTORY, "Factory"),
+        (DISTRIBUTOR, "Distributor"),
+        (DEALERSHIP, "Dealership"),
+        (RETAIL, "Retail"),
+        (INDIVIDUAL, "Individual"),
+    ]
+    name = models.CharField(max_length=50)
+    company_type = models.CharField(max_length=50, choices=ELEMENT_TYPES)
+    supplier = models.ForeignKey('self', on_delete=models.PROTECT, null=True, blank=True)
+    debt_to_supplier = models.DecimalField(max_digits=11, decimal_places=2, blank=True, null=True)
+    create_date = models.DateField(auto_now_add=True)
+    level = models.PositiveSmallIntegerField()
+
+    def _factory_type_validate(self):
+        self.level = 0
+        if self.debt_to_supplier is not None:
+            raise ValidationError({'debt_to_supplier': ERROR_MESSAGES['wrong_factory_debt']})
+
+    def _other_types_validate(self):
+        if self.supplier:
+            self.level = self.supplier.level + 1
+            if not self.debt_to_supplier:
+                self.debt_to_supplier = 0
+        else:
+            raise ValidationError({'supplier': ERROR_MESSAGES['should_has_supplier']})
+
+    def clean(self):
+        if self.company_type == self.FACTORY:
+            self._factory_type_validate()
+        else:
+            self._other_types_validate()
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.get_company_type_display()} - {self.name}'
