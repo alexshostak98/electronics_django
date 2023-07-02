f24b48b6aa2c0f15d3b1eec6f6c0447d5780ad5f
alexshostak98 <alexandr_shostak@mail.ru>
2023-07-02 15:29:39 +0300

    start djangorestframework

--- electronics_sales/settings.py
+++ electronics_sales/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'company',
     'contacts',
     'product',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- electronics_sales/urls.py
+++ electronics_sales/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from api.router import api_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('electronics-sales', include((api_router.urls, 'api'))),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
