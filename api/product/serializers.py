b2680d977a9c921d864892a8ab025ace8b1033ce
alexshostak98 <alexandr_shostak@mail.ru>
2023-07-03 11:55:15 +0300

    create product view and serializer

---
+++ api/product/serializers.py
@@ -0,0 +1,28 @@
+import datetime
+from rest_framework import serializers
+from company.models import Company
+from product.models import Product
+from product.messages import ERROR_MESSAGES
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset=Product.objects.all(),
+                fields=['name', 'model'],
+            )
+        ]
+
+    def validate_market_launch_date(self, value):
+        first_company_creation_date = Company.objects.get(level__exact=0).creation_date.date()
+        current_date = datetime.date.today()
+        if value > current_date:
+            raise serializers.ValidationError(ERROR_MESSAGES['gt_current_date'])
+        elif value < first_company_creation_date:
+            raise serializers.ValidationError(
+                ERROR_MESSAGES['gt_first_company_date'].format(first_company_creation_date),
+            )
+        return value
