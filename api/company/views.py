32a2c3fb58d0ab5ea6c386e5069a7f160807d098
alexshostak98 <alexandr_shostak@mail.ru>
2023-07-04 22:34:02 +0300

    add action send email with qr code

--- api/company/views.py
+++ api/company/views.py
@@ -1,13 +1,17 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Avg
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from api.company import serializers
 from api.company.filters import CompanyFilter
 from company.models import Company
+from contacts.models import Email
+from company.messages import SUCCESS_MESSAGES, ERROR_MESSAGES
+from company.tasks import send_email_with_qr_code
 
 
-class CompanyViewSet(ModelViewSet):
+class CompanyViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.CreateUpdateCompanySerializer
     queryset = Company.objects.all()
     action_serializers = {
@@ -23,5 +27,17 @@ class CompanyViewSet(ModelViewSet):
     def debt_above_average(self, request, *args, **kwargs):
         queryset = self.get_queryset()
         average_debt = queryset.exclude(debt_to_supplier__isnull=True).aggregate(avg_debt=Avg('debt_to_supplier'))
-        companies_with_above_average_debt = queryset.filter(debt_to_supplier__gt=average_debt["avg_debt"])
+        companies_with_above_average_debt = queryset.filter(debt_to_supplier__gt=average_debt['avg_debt'])
         return Response(self.get_serializer(companies_with_above_average_debt, many=True).data)
+
+    @action(methods=['get'], detail=True, url_path='send-qrcode-to-email', name='Send QR code to email')
+    def send_qr_code_with_company_contacts_to_email(self, request, pk):
+        company = self.get_object()
+        try:
+            contacts = company.contacts
+        except ObjectDoesNotExist:
+            return Response({'status', ERROR_MESSAGES['has_not_email']})
+        emails = [email_data.email for email_data in Email.objects.filter(contacts=contacts.pk)]
+        contacts_text = f"{contacts.address}\n{', '.join(emails)}"
+        send_email_with_qr_code.delay(contacts_text, company.name, emails)
+        return Response({'status': SUCCESS_MESSAGES['send_qr_code_to_email']})
--- company/messages.py
+++ company/messages.py
@@ -2,9 +2,11 @@ ERROR_MESSAGES = {
     'wrong_factory_debt': 'Factory cannot have a debt, even 0. Leave this field blank or choose another company type',
     'should_has_supplier': 'This company type should has a supplier. Please choose the supplier',
     'positive_debt': 'Debt must be positive',
-    'factory_supplier': 'Company with Factory type cannot has a supplier'
+    'factory_supplier': 'Company with Factory type cannot has a supplier',
+    'has_not_email': 'This company has not contacts information and any emails',
 }
 
 SUCCESS_MESSAGES = {
     'reset_debt': 'Debt to supplier for selected companies has been successfully reset',
+    'send_qr_code_to_email': 'QR code with company contacts has been generated and sent to your emails',
 }
