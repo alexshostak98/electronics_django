dfd68b087824e794406cc3d14b382169973a856f
alexshostak98 <alexandr_shostak@mail.ru>
2023-07-05 08:34:53 +0300

    statr js button

---
+++ company/static/company/js/button_copy.js
@@ -0,0 +1,1 @@
+var copyBtns = document.getElementById('id_contacts-0-email')
--- electronics_sales/settings.py
+++ electronics_sales/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'electronics_sales.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,6 +130,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'company/static')]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
