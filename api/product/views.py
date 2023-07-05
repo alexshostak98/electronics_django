615c9f02df825ea8f8a891f9ce45b439eda4a257
alexshostak98 <alexandr_shostak@mail.ru>
2023-07-05 03:01:39 +0300

    add permission to product view

--- api/product/views.py
+++ api/product/views.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from api.product.serializers import ProductSerializer
 from product.models import Product
+from rest_framework.permissions import IsAdminUser
 
 
 class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    permission_classes = [IsAdminUser]
