e1d2575c846b7a50026fa891c483992c3043c3ef
alexshostak98 <alexandr_shostak@mail.ru>
2023-07-06 08:35:18 +0300

    add js button to copy email to clipboard

--- company/static/company/js/button_copy.js
+++ company/static/company/js/button_copy.js
@@ -1,1 +1,17 @@
-var copyBtns = document.getElementById('id_contacts-0-email')
+var copyBtn = document.getElementById('copy_emails_btn')
+console.log(copyBtn)
+
+var all_emails = ''
+
+copyBtn.addEventListener('click', () => {
+    console.log('click')
+    var emails_data = document.getElementById('id_contacts-0-email').childNodes
+    for (var email_data of emails_data) {
+        if (email_data.nodeName === 'OPTION' && email_data.hasAttribute('selected')){
+            all_emails += email_data.textContent + '\n'
+        }
+        }
+    console.log(all_emails)
+    navigator.clipboard.writeText(all_emails)
+    all_emails = ''
+})
